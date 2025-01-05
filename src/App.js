298868544da4cb33fd32b06d89463b47c0202f03
unknown <venkateshk5764@gmail.com>
2025-01-05 12:13:37 +0530

    05-01-2025 commit

--- src/App.js
+++ src/App.js
@@ -12,4 +12,30 @@ function Bulb(){
     </>
   );
 }
-export default Bulb;
+function Hookprac(){
+  const [isbold,undda]=useState(false);
+  const [isitalic,ayinda]=useState(false);
+  const boldlopettu=()=>{ undda(!isbold);};
+  const itlaicchudu=()=>{ayinda(!isitalic);};
+  return(
+    <>
+    <h1 style={{fontWeight: isbold? 'bold':'normal',fontStyle: isitalic? 'italic':'normal'}}>
+      This is the practice session of hooks
+    </h1>
+    <button onClick={boldlopettu}>BOLD</button>
+    <button onClick={itlaicchudu}>ITALIC</button>
+    </>
+  );
+}
+function Geeta(){
+  const [isgita,undi]=useState(false);
+  const gitunda=()=>{undi(!isgita);};
+  return(
+    <>
+    <h1 style={{color:isgita? 'red' : 'green'}}> UnderlIne Hook Practice</h1>
+    <button onClick={gitunda}>Daku Maharaj</button>
+    </>
+  );
+}
+export {Bulb,Hookprac,Geeta};
+
